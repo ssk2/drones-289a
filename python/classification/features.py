27e39cf96923391376a65071a135094a77d6d488
chrisechanique <chrisechanique@airbears2-10-142-5-227.airbears2.1918.berkeley.edu>
2014-05-08 13:23:41 -0700

    added numpy to functions

--- python/classification/features.py
+++ python/classification/features.py
@@ -17,19 +17,19 @@ def histogram(data, bins):
 
 def top_frequencies(data, N):
     sorted_data = sorted(data, key=operator.itemgetter(3))
-    top_freqs = asarray(sorted_data[-N:])
+    top_freqs = numpy.asarray(sorted_data[-N:])
     return top_freqs[:,0]
 
 def centroid(data):
     Z = data[:,3]
     Z_norm = Z*1.0/sum(Z)
-    return mean(Z_norm)
+    return numpy.mean(Z_norm)
 
 def feature_vector(data, hist_bins, top_freqs):
     hist_vect = histogram(data, hist_bins)
     freq_vect = top_frequencies(data, top_freqs)
     cent_vect = centroid(data)
-    return concatenate((hist_vect, freq_vect, [cent_vect]))
+    return numpy.concatenate((hist_vect, freq_vect, [cent_vect]))
 
 def normalize_data(data):
     Z = data[:,3]
