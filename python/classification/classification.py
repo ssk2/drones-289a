c18802db263f29e5b27d0a9fd169167da3dca6bc
chrisechanique <chrisechanique@airbears2-10-142-4-159.airbears2.1918.berkeley.edu>
2014-05-09 15:43:34 -0700

    added extra feature vector functions and updated classification file

--- python/classification/classification.py
+++ python/classification/classification.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[6]:
-
 import classification.features as features
 reload(classification.features)
 
@@ -14,21 +10,24 @@ import data.fetch
 reload (data.fetch)
 from sklearn import svm
 
-train_indices, test_indices = data.fetch.get_sample_indices(50, 300)
+# PARAMETERS
+bins = 10 # this currently does nothing, histogram fv commented out
+top_n_freq = 2
+xyz_set = [3] # X = 1, Y = 2, Z = 3... i.e. [1,3] uses X and Z of data for fv
+train_sample_size = 300
+# ^^^ test_sample_size = 350 - train_sample_size
+
+train_indices, test_indices = data.fetch.get_sample_indices(train_sample_size, 50)
 train_data = data.fetch.get_sample_data (train_indices)
 test_data = data.fetch.get_sample_data (test_indices)
 
-X,y = features.generate_training_set(train_data, 10, 5)
-Xtest,ytest = features.generate_training_set(test_data, 10, 5)
+X,y = features.generate_feature_vectors(train_data, xyz_set, bins, top_n_freq)
+Xtest,ytest = features.generate_feature_vectors(test_data, xyz_set, bins, top_n_freq)
 
 clf = svm.SVC()
 clf.fit(X,y)
 
 ypred = clf.predict(Xtest)
 print 100*(sum(ypred == ytest))/len(ytest)
-
-
-# In[ ]:
-
 
 
--- python/classification/features.py
+++ python/classification/features.py
@@ -8,41 +8,74 @@ import operator
 import random
 import matplotlib.pyplot as plt
 
-def histogram(data, bins):
-    freq = data[:,0]
-    Z = data[:,3]
-    H = numpy.asarray(plt.hist(Z,bins,(freq[0],freq[-1])))
-    H[0] = 1.0*H[0]/sum(H[0])
-    return H[0]
+def histogram(data, data_type, bins):
+    A = data[:,data_type]
+    binlength = int(math.floor(len(data)/bins))
+    binned_data = zeros(bins)
+    for i in range(0, bins):
+        summ = 0
+        for j in range(0,binlength):
+            summ = summ + A[j+i*binlength]
+        binned_data[i] = summ
+    normalized_data = binned_data*1.0/numpy.sum(binned_data)
+    return normalized_data
 
-def top_frequencies(data, N):
-    sorted_data = sorted(data, key=operator.itemgetter(3))
+def top_frequencies(data, data_type, N):
+    sorted_data = sorted(data, key=operator.itemgetter(data_type))
     top_freqs = numpy.asarray(sorted_data[-N:])
     return top_freqs[:,0]
 
-def centroid(data):
-    Z = data[:,3]
-    Z_norm = Z*1.0/sum(Z)
-    return numpy.mean(Z_norm)
+def data_mean(data, data_type):
+    A = data[:,data_type]
+    A_norm = A*1.0/sum(A)
+    A_mean = numpy.mean(A_norm)
+    return [A_mean]
 
-def feature_vector(data, hist_bins, top_freqs):
-    hist_vect = histogram(data, hist_bins)
-    freq_vect = top_frequencies(data, top_freqs)
-    cent_vect = centroid(data)
-    return numpy.concatenate((hist_vect, freq_vect, [cent_vect]))
+def peak_ratio(data, data_type):
+    A = data[:,data_type]
+    A_norm = A*1.0/sum(A)
+    peak_magnitude = max(A_norm)
+    mean_magnitude = data_mean(data, data_type)
+    if mean_magnitude <= 0:
+        return -1
+    return peak_magnitude/mean_magnitude
 
-def normalize_data(data):
-    Z = data[:,3]
-    Z_norm = Z*1.0/sum(Z)
-    data[:,3] = Z_norm
+def centroid(data, data_type):
+    freq = data[:,0]
+    A = data[:,data_type]
+    A_norm = A*1.0/sum(A)
+    n = len(A)
+    summ = 0
+    for i in range(0,n):
+        if(summ + A_norm[i] > 0.5):
+            return [freq[i]]
+        summ = summ + A_norm[i]
+    return [-1]
+
+def feature_vector(data, data_type, hist_bins, top_freqs):
+    hist_vect = histogram(data, data_type, hist_bins)
+    freq_vect = top_frequencies(data, data_type, top_freqs)
+    cent_vect = centroid(data, data_type)
+    ratio_vect = peak_ratio(data, data_type)
+    return numpy.concatenate((freq_vect, cent_vect, ratio_vect))
+
+def normalize_data(data, data_type):
+    A = data[:,data_type]
+    A_norm = Z*1.0/sum(Z)
+    data[:,data_type] = A_norm
     return data
 
-def generate_training_set(dataset, histbins, topfreq):
+def generate_feature_vectors(dataset, data_types, histbins, topfreq):
     trainlabels = []
     trainset = []
     samplesize = len(dataset)
+    num_types = len(data_types)
     for i in range(0,samplesize):
         trainlabels.append(dataset[i][0])
-        trainset.append(feature_vector(dataset[i][1], histbins, topfreq))
+        fullfeaturevector = []
+        for j in data_types:
+            vect = feature_vector(dataset[i][1], j, histbins, topfreq)
+            fullfeaturevector = numpy.concatenate((fullfeaturevector, vect))
+        trainset.append(fullfeaturevector)
     return (trainset, trainlabels)
 
