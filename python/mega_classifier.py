e413d3a90f633686420b823daca1af0826dfc08b
Sunil Shah <git@ss21k.co.uk>
2014-05-12 17:06:36 -0700

    Using ensemble methods

--- python/mega_classifier.py
+++ python/mega_classifier.py
@@ -3,16 +3,11 @@ from data import fetch
 from data import load
 from classification import features
 from numpy import mean
-from sklearn import svm, neighbors
+from sklearn import svm, neighbors, tree, ensemble
 
 
 # cross validation properties
 folds = 5
-
-# feature vector properties
-bin_width = 50
-top_freqs = 1
-xyz_set = [3] # X = 1, Y = 2, Z = 3... i.e. [1,3] uses X and Z of data for fv
 
 def fit_and_predict(clf, name, cv_sets):
 	print "using %s classifier" % name
@@ -22,8 +17,8 @@ def fit_and_predict(clf, name, cv_sets):
 		train_indices, test_indices = cv_sets[current_fold]
 		train_data = fetch.get_sample_data (train_indices)
 		test_data = fetch.get_sample_data (test_indices)
-		Xtest,ytest = features.generate_feature_vectors(test_data, xyz_set, bin_width, top_freqs)
-		X,y = features.generate_feature_vectors(train_data, xyz_set, bin_width, top_freqs)
+		Xtest,ytest = features.generate_feature_vectors(test_data, xyz_set, bins, top_n_freq, centroid_bool, ratio_bool, num_peaks_bool)
+		X,y = features.generate_feature_vectors(train_data, xyz_set, bins, top_n_freq, centroid_bool, ratio_bool, num_peaks_bool)
 		clf.fit(X,y)
 		y_train_pred = clf.predict(X)
 		train_accuracies.append(100*(sum(y_train_pred == y))/len(y))
@@ -40,46 +35,101 @@ def fit_and_predict(clf, name, cv_sets):
 # svm properties
 
 # Use cross validation to get different subsets pulse_width = pulse_width, unloaded = unloaded
-cv_sets  = fetch.get_sample_indices_for_crossvalidation(folds = folds)
+cv_sets  = fetch.get_sample_indices_for_crossvalidation(folds = folds, multiclass = "false")
+
+
+# try different feature vectors
+clf = svm.SVC()
+
+# just top_n_freq
+bins = 0
+top_n_freq = 1
+centroid_bool = 0
+ratio_bool = 0
+num_peaks_bool = 0
+xyz_set = [3] # X = 1, Y = 2, Z = 3... i.e. [1,3] uses X and Z of data for fv
+
+# fit_and_predict(clf, "SVM: only top_n_freq", cv_sets)
+
+# just ratio
+top_n_freq = 0
+ratio_bool = 1
+
+# fit_and_predict(clf, "SVM: only ratio", cv_sets)
+
+
+# just centroid
+bins = 0
+centroid_bool = 1
+
+# fit_and_predict(clf, "SVM: only centroid", cv_sets)
+
+
+# just num peaks
+
+
+# different bin_widths
+
+
+# different axes
+
+
+
+
 
 # Try different methods: http://scikit-learn.org/stable/tutorial/machine_learning_map/index.html
 
-# Try ensemble methods: http://scikit-learn.org/stable/modules/ensemble.htmlex
+bins = 10
+top_n_freq = 1
+centroid_bool = 1
+ratio_bool = 1
+num_peaks_bool = 1
+xyz_set = [1,2,3] 
 
-# try different feature vectors
+### Decision Tree : http://scikit-learn.org/stable/modules/tree.html
+# clf = tree.DecisionTreeClassifier()
+# fit_and_predict(clf, "Decision Tree", cv_sets)
 
-# try different training methods
+### Ensemble Methods : http://scikit-learn.org/stable/modules/ensemble.html
+# try different n_estimators
+clf = ensemble.RandomForestClassifier(n_estimators = 10)
+fit_and_predict(clf, "Random Forest", cv_sets)
+
+# Adaboost
+clf = ensemble.AdaBoostClassifier(n_estimators=10)
+fit_and_predict(clf, "AdaBoost", cv_sets)
 
 ### SVM : http://scikit-learn.org/stable/modules/svm.html#classification 
 
 # try different kernels
-# 'poly' too slow
-for kernel_type in ('precomputed', 'linear', 'rbf', 'sigmoid'):
-	clf = svm.SVC(kernel=kernel_type)
-	name = "SVM with %s kernel" % kernel_type
-	fit_and_predict(clf, name, cv_sets)		
+# 'poly', 'linear' too slow 
+# 'precomputed' doesn't work
+# for kernel_type in ('rbf', 'sigmoid'):
+# 	clf = svm.SVC(kernel=kernel_type)
+# 	name = "SVM with %s kernel" % kernel_type
+# 	fit_and_predict(clf, name, cv_sets)		
 
 # effect of different C values
-for c in range (0, 1, 0.1):
-	clf = svm.SVC(C=c)
-	name = "SVM with %d C hyperparameter" % c
-	fit_and_predict(clf, name, cv_sets)
+# for c in range (1, 10, 1):
+# 	clf = svm.SVC(C=c/10)
+# 	name = "SVM with %d C hyperparameter" % c
+# 	fit_and_predict(clf, name, cv_sets)
 
 
 ### k-NN : http://scikit-learn.org/stable/modules/neighbors.html
 
 # try different weights
-for weight in  ('uniform' 'distance'):
-	clf = neighbors.KNeighborsClassifier(1, weights=weight)
-	name = "k-NN with %s weights" % weight
-	fit_and_predict(clf, name, cv_sets)
+# for weight in  ('uniform', 'distance'):
+# 	clf = neighbors.KNeighborsClassifier(1, weights=weight)
+# 	name = "k-NN with %s weights" % weight
+# 	fit_and_predict(clf, name, cv_sets)
 
 
 # try different neighbors
-for n_neighbours in  range(10):
-	clf = neighbors.KNeighborsClassifier(n_neighbours)
-	name = "k-NN with %d neighbours" % n_neighbours
-	fit_and_predict(clf, name, cv_sets)
+# for n_neighbours in range(1, 10):
+# 	clf = neighbors.KNeighborsClassifier(n_neighbours)
+# 	name = "k-NN with %d neighbours" % n_neighbours
+# 	fit_and_predict(clf, name, cv_sets)
 
 
 
